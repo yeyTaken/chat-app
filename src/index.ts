52789276a43235e0c8e55ac6428a2efe54086367
Rabi <yeytaken.ofc@gmail.com>
2025-02-27 12:50:51 +0000

    Feat: whatsapp bot

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-import { initializeClient } from "./whatsapp/client";
+import { Message } from "whatsapp-web.js";
+import { client } from "./whatsapp/client";
 
-initializeClient();
+client.on('message_create', message => {
+	if (message.body === '!ping') {
+		// send back "pong" to the chat the message was sent in
+		client.sendMessage(message.from, 'pong');
+	}
+});
+
+
+client.initialize();
--- src/whatsapp/client/index.ts
+++ src/whatsapp/client/index.ts
@@ -1,36 +1,15 @@
 import { Client, LocalAuth } from "whatsapp-web.js";
-// import qrcodeTerminal from "qrcode-terminal";
-import QRCode from "qrcode";
+import qrcode from "qrcode-terminal";
 
-import { registerCommands, loadCommands } from "../handler/commands.handler";
+export const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: { headless: true },
+});
 
-export async function initializeClient(): Promise<void> {
-  const client = new Client({
-    authStrategy: new LocalAuth(),
-  });
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
 
-  client.on("qr", async (qr) => {
-    console.log("QR Code recebido, escaneie com o WhatsApp:");
-    // qrcodeTerminal.generate(qr, { small: true });
-    
-    try {
-      await QRCode.toFile("qrcode.png", qr);
-      console.log("QR Code salvo como 'qrcode.png'. Abra para escanear!");
-    } catch (error) {
-      console.error("Erro ao gerar a imagem do QR Code:", error);
-    }
-  });
-
-  client.on("ready", async () => {
-    console.log("Cliente pronto!");
-    try {
-      const commands = await loadCommands();
-      registerCommands(client, commands);
-      console.log(`Registrados ${commands.length} comando(s).`);
-    } catch (error) {
-      console.error("Erro ao carregar os comandos:", error);
-    }
-  });
-
-  client.initialize();
-}
+client.on("ready", () => {
+  console.log("Cliente está pronto!");
+});
